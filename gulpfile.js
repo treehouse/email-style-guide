12f2668b81fbe8421ae73bd0d0d1d898831cb792
Andrew Olson <anolson@gmail.com>
2017-08-01 16:48:45 -0400

    Add gulp + initial tasks.

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var merge = require('merge-stream');
+
+var sass = require('gulp-sass');
+
+var folder = {
+  src: 'src/',
+  dest: 'dist/',
+  docsSrc: 'docs/src/',
+  docsDest: 'docs/dist/',
+};
+
+gulp.task('css', function() {
+  var src = folder.src + 'sass/email-style.sass';
+  var dest = folder.dest + 'css/';
+  var sassOpts = {
+    outputStyle: 'expanded',
+    includePaths: ['bower_components/project-leap/_sass'],
+    precision: 3
+  };
+
+  gulp.src(src)
+    .pipe(sass(sassOpts)
+    .on('error', sass.logError))
+    .pipe(gulp.dest(dest));
+});
+
+
+gulp.task('docs:css', function() {
+  var src = folder.docsSrc + 'sass/styleguide.sass';
+  var dest = folder.docsDest + 'css/';
+  var sassOpts = {
+    outputStyle: 'expanded',
+    includePaths: ['bower_components/project-leap/_sass', 'src/sass'],
+    precision: 3
+  };
+
+  gulp.src(src)
+    .pipe(sass(sassOpts)
+    .on('error', sass.logError))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('default', ['src', 'docs']);
+gulp.task('src', ['css']);
+gulp.task('docs', ['docs:css']);
